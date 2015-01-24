21f09e068b77d8348b0585667598a6bddef9989e
Andrew McPherson <andrewmcp333@gmail.com>
2015-01-24 00:23:52 -0500

    Implemented a system of physics for the players.

--- source/scripts/stores/PlayerStore.js
+++ source/scripts/stores/PlayerStore.js
@@ -1,4 +1,9 @@
+var LoopActions = require("<scripts>/actions/LoopActions")
 var PlayerActions = require("<scripts>/actions/PlayerActions")
+
+var acceleration = 1
+var deacceleration = 0.25
+var maximum_velocity = 3
 
 var PlayerStore = Reflux.createStore({
     data: [
@@ -6,6 +11,10 @@ var PlayerStore = Reflux.createStore({
             position: {
                 x: 3,
                 y: 3
+            },
+            velocity: {
+                x: 0,
+                y: 0
             }
         }
     ],
@@ -13,26 +22,62 @@ var PlayerStore = Reflux.createStore({
         return this.data
     },
     listenables: [
+        LoopActions,
         PlayerActions
     ],
-    onPlayerMoveNorth: function(tick)
-    {
-        this.data[0].position.y -= 1 * tick
-        this.retrigger()
+    onPlayerMoveNorth: function(tick) {
+        this.data[0].velocity.y -= acceleration * tick
+        if(this.data[0].velocity.y < -maximum_velocity) {
+            this.data[0].velocity.y = -maximum_velocity
+        }
     },
-    onPlayerMoveSouth: function(tick)
-    {
-        this.data[0].position.y += 1 * tick
-        this.retrigger()
+    onPlayerMoveSouth: function(tick) {
+        this.data[0].velocity.y += acceleration * tick
+        if(this.data[0].velocity.y > +maximum_velocity) {
+            this.data[0].velocity.y = +maximum_velocity
+        }
     },
-    onPlayerMoveWest: function(tick)
-    {
-        this.data[0].position.x -= 1 * tick
-        this.retrigger()
+    onPlayerMoveWest: function(tick) {
+        this.data[0].velocity.x -= acceleration * tick
+        if(this.data[0].velocity.x < -maximum_velocity) {
+            this.data[0].velocity.x = -maximum_velocity
+        }
     },
-    onPlayerMoveEast: function(tick)
-    {
-        this.data[0].position.x += 1 * tick
+    onPlayerMoveEast: function(tick) {
+        this.data[0].velocity.x += acceleration * tick
+        if(this.data[0].velocity.x > +maximum_velocity) {
+            this.data[0].velocity.x = +maximum_velocity
+        }
+    },
+    onTick: function(tick) {
+        var datum = this.data[0]
+        datum.position.x += datum.velocity.x * tick
+        datum.position.y += datum.velocity.y * tick
+
+        if(datum.velocity.x < 0) {
+            datum.velocity.x += deacceleration * tick
+            if(datum.velocity.x > 0) {
+                datum.velocity.x = 0
+            }
+        }
+        else if(datum.velocity.x > 0) {
+            datum.velocity.x -= deacceleration * tick
+            if(datum.velocity.x < 0) {
+                datum.velocity.x = 0
+            }
+        }
+        if(datum.velocity.y < 0) {
+            datum.velocity.y += deacceleration * tick
+            if(datum.velocity.y > 0) {
+                datum.velocity.y = 0
+            }
+        }
+        else if(datum.velocity.y > 0) {
+            datum.velocity.y -= deacceleration * tick
+            if(datum.velocity.y < 0) {
+                datum.velocity.y = 0
+            }
+        }
         this.retrigger()
     }
 })
