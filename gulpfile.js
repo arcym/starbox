2d879332b104a9cf12f8db26cbd1ab94c5e97dff
Andrew McPherson <andrewmcp333@gmail.com>
2015-01-23 20:56:49 -0500

    Configured gulp to watch for changes.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 var gulp = require("gulp")
 var gulp_sass = require("gulp-sass")
+var gulp_util = require("gulp-util")
 var gulp_prefixify_css = require("gulp-autoprefixer")
 var gulp_minify_css = require("gulp-minify-css")
 var gulp_minify_html = require("gulp-minify-html")
 var gulp_json_transform = require("gulp-json-transform")
+var gulp_livereload = require("gulp-livereload")
 var gulp_uglify = require("gulp-uglify")
 var gulp_if = require("gulp-if")
 
+var chalk = require("chalk")
+var server = require("pushstate-server")
 var vinyl_buffer = require("vinyl-buffer")
 var vinyl_source = require("vinyl-source-stream")
 
@@ -32,19 +36,23 @@ gulp.task("scripts", function()
            }
         }))
         .bundle()
+        .on('error', on_error)
         .pipe(vinyl_source("index.js"))
         .pipe(vinyl_buffer())
         .pipe(gulp_if(is_gh_pages(), gulp_uglify()))
         .pipe(gulp.dest("./builds"))
+        .pipe(gulp_livereload())
 });
 
 gulp.task("styles", function()
 {
     gulp.src("./source/index.scss")
         .pipe(gulp_sass())
+        .on('error', on_error)
         .pipe(gulp_prefixify_css())
         .pipe(gulp_if(is_gh_pages(), gulp_minify_css()))
         .pipe(gulp.dest("./builds"))
+        .pipe(gulp_livereload())
 })
 
 gulp.task("markup", function()
@@ -52,12 +60,14 @@ gulp.task("markup", function()
     gulp.src("./source/index.html")
         .pipe(gulp_if(is_gh_pages(), gulp_minify_html()))
         .pipe(gulp.dest("./builds"))
+        .pipe(gulp_livereload())
 })
 
 gulp.task("assets", function()
 {
     gulp.src("./source/assets/**/*", {base: "./source"})
         .pipe(gulp.dest("./builds"))
+        .pipe(gulp_livereload())
 })
 
 gulp.task("configs", function()
@@ -74,7 +84,22 @@ gulp.task("configs", function()
 
 gulp.task("default", ["scripts", "styles", "markup", "assets", "configs"]);
 
+gulp.task("watch", function()
+{
+    server.start({port: 1271, directory: "./builds"})
+    gulp_livereload.listen()
+
+    gulp.watch("./source/**/*.js", ["scripts"])
+    gulp.watch("./source/**/*.scss", ["styles"])
+})
+
 function is_gh_pages()
 {
     return process.env.platform == "gh_pages"
+}
+
+function on_error()
+{
+    gulp_util.log(chalk.bold.red(error.message))
+    gulp_util.beep()
 }
